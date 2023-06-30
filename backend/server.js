f2cd93e770c677e146b1bf4416da55f947945d56
ReicaD <dbamanya447@gmail.com>
2023-06-30 15:48:53 -0400

    Modified with Routes and ENV

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 6000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(202).json({ message: "Welcome to service Assist" });
+});
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
